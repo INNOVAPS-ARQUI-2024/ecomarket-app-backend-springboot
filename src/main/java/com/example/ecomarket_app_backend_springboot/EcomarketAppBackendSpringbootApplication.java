3eeca136b2ef74270020128452eec5cbbda16b5d
XxDaniel-AlzatexX <daniel-alzatem@javeriana.edu.co>
2024-09-20 00:52:43 -0500

    :feat-feat:Creación del proyecto base

---
+++ src/main/java/com/example/ecomarket_app_backend_springboot/EcomarketAppBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecomarket_app_backend_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomarketAppBackendSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomarketAppBackendSpringbootApplication.class, args);
+	}
+
+}
