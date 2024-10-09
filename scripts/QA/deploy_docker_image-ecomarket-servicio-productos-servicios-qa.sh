7d3e2a600b711a0295ea4fe70eb4471cdcea5e17
Esteban Hernando Pedraza Solano <estemanps99@gmail.com>
2024-10-09 16:16:41 -0500

    Update deploy_docker_image-ecomarket-servicio-productos-servicios-qa.sh

--- scripts/QA/deploy_docker_image-ecomarket-servicio-productos-servicios-qa.sh
+++ scripts/QA/deploy_docker_image-ecomarket-servicio-productos-servicios-qa.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Variables (puedes personalizarlas o recibirlas como parámetros)
+# Variables
 DOCKER_IMAGE_PATH="/tmp/ecomarket-servicio-productos-servicios-qa.tar"
 DOCKER_IMAGE_NAME="ecomarket-servicio-productos-servicios:qa"
 CONTAINER_NAME="ecomarket-servicio-productos-servicios-qa"
+NETWORK_NAME="ecomarket-network"
 
 # Cargar la imagen Docker
 docker load -i $DOCKER_IMAGE_PATH
@@ -12,11 +13,10 @@ docker load -i $DOCKER_IMAGE_PATH
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
-# Ejecutar un nuevo contenedor
-docker run -d --name $CONTAINER_NAME -p 8080:8080 $DOCKER_IMAGE_NAME
+# Ejecutar un nuevo contenedor en la red (sin exponer puerto al host)
+docker run -d --name $CONTAINER_NAME --network $NETWORK_NAME $DOCKER_IMAGE_NAME
 
 # Opcional: eliminar el archivo .tar para ahorrar espacio
 rm $DOCKER_IMAGE_PATH
 
 echo "Deployment ecomarket-servicio-productos-servicios-qa completed successfully!"
-
