f25ba001fdbbd909d2f4f86300cd5adaa8f176a8
Daniel Felipe Alzate <117049972+XxDaniel-AlzatexX@users.noreply.github.com>
2024-10-09 02:02:40 -0500

    Update deploy_docker_image-ecomarket-servicio-eureka-qa.sh

--- scripts/QA/deploy_docker_image-ecomarket-servicio-eureka-qa.sh
+++ scripts/QA/deploy_docker_image-ecomarket-servicio-eureka-qa.sh
@@ -13,7 +13,7 @@ docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
 # Ejecutar un nuevo contenedor
-docker run -d --name $CONTAINER_NAME -p 80:80 $DOCKER_IMAGE_NAME
+docker run -d --name $CONTAINER_NAME -p 8761:8761 $DOCKER_IMAGE_NAME
 
 # Opcional: eliminar el archivo .tar para ahorrar espacio
 rm $DOCKER_IMAGE_PATH
