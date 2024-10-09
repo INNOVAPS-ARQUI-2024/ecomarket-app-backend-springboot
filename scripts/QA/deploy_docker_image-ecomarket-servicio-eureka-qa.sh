b7ffa80fa10eb771aa425fe358bf8f526d4c003c
Esteban Hernando Pedraza Solano <estemanps99@gmail.com>
2024-10-09 01:27:18 -0500

    Create deploy_docker_image-ecomarket-servicio-eureka-qa.sh

---
+++ scripts/QA/deploy_docker_image-ecomarket-servicio-eureka-qa.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Variables
+DOCKER_IMAGE_PATH="/tmp/ecomarket-servicio-eureka-qa.tar"
+DOCKER_IMAGE_NAME="ecomarket-app-frontend:qa"
+CONTAINER_NAME="ecomarket-servicio-eureka-qa"
+
+# Cargar la imagen Docker
+docker load -i $DOCKER_IMAGE_PATH
+
+# Detener y eliminar el contenedor anterior si existe
+docker stop $CONTAINER_NAME || true
+docker rm $CONTAINER_NAME || true
+
+# Ejecutar un nuevo contenedor
+docker run -d --name $CONTAINER_NAME -p 80:80 $DOCKER_IMAGE_NAME
+
+# Opcional: eliminar el archivo .tar para ahorrar espacio
+rm $DOCKER_IMAGE_PATH
+
+echo "Deployment ecomarket-servicio-eureka-qa completed successfully!"
