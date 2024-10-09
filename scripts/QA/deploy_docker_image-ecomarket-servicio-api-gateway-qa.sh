322e32412986bebbe56e71d9a6d79061dce59262
Daniel Felipe Alzate <117049972+XxDaniel-AlzatexX@users.noreply.github.com>
2024-10-09 01:25:03 -0500

    Create deploy_docker_image-ecomarket-servicio-api-gateway-qa.sh

---
+++ scripts/QA/deploy_docker_image-ecomarket-servicio-api-gateway-qa.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Variables (puedes personalizarlas o recibirlas como parámetros)
+DOCKER_IMAGE_PATH="/tmp/ecomarket-servicio-api-gateway-qa.tar"
+DOCKER_IMAGE_NAME="ecomarket-servicio-api-gateway:qa"
+CONTAINER_NAME="ecomarket-servicio-api-gateway-qa"
+
+# Cargar la imagen Docker
+docker load -i $DOCKER_IMAGE_PATH
+
+# Detener y eliminar el contenedor anterior si existe
+docker stop $CONTAINER_NAME || true
+docker rm $CONTAINER_NAME || true
+
+# Ejecutar un nuevo contenedor
+docker run -d --name $CONTAINER_NAME -p 7999:8080 $DOCKER_IMAGE_NAME
+
+# Opcional: eliminar el archivo .tar para ahorrar espacio
+rm $DOCKER_IMAGE_PATH
+
+echo "Deployment ecomarket-servicio-api-gateway-qa completed successfully!"
