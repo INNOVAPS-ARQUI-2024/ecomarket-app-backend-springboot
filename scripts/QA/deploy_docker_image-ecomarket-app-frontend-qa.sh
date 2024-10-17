7c0de082b3e49b76bc129e39521e96c29f223f12
Esteban Hernando Pedraza Solano <estemanps99@gmail.com>
2024-10-17 18:46:50 -0500

    Update deploy_docker_image-ecomarket-app-frontend-qa.sh

--- scripts/QA/deploy_docker_image-ecomarket-app-frontend-qa.sh
+++ scripts/QA/deploy_docker_image-ecomarket-app-frontend-qa.sh
@@ -4,6 +4,7 @@
 DOCKER_IMAGE_PATH="/tmp/ecomarket-app-frontend-qa.tar"
 DOCKER_IMAGE_NAME="ecomarket-app-frontend:qa"
 CONTAINER_NAME="ecomarket-app-frontend-qa"
+NETWORK_NAME="ecomarket-network"
 
 # Cargar la imagen Docker
 docker load -i $DOCKER_IMAGE_PATH
@@ -13,10 +14,9 @@ docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
 # Ejecutar un nuevo contenedor
-docker run -d --name $CONTAINER_NAME -p 80:80 $DOCKER_IMAGE_NAME
+docker run -d --name $CONTAINER_NAME --network $NETWORK_NAME -p 80:80 $DOCKER_IMAGE_NAME --env_file 
 
 # Opcional: eliminar el archivo .tar para ahorrar espacio
 rm $DOCKER_IMAGE_PATH
 
 echo "Deployment ecomarket-app-frontend-qa completed successfully!"
-
