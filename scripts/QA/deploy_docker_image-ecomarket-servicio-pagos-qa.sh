34b51b194e0309d746a4389fab27ab39b3e509dc
Daniel Felipe Alzate <117049972+XxDaniel-AlzatexX@users.noreply.github.com>
2024-10-17 21:07:07 -0500

    Update deploy_docker_image-ecomarket-servicio-pagos-qa.sh

--- scripts/QA/deploy_docker_image-ecomarket-servicio-pagos-qa.sh
+++ scripts/QA/deploy_docker_image-ecomarket-servicio-pagos-qa.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 # Variables
-DOCKER_IMAGE_PATH="/tmp/ecomarket-servicio-pagos-qa.tar"
 DOCKER_IMAGE_NAME="ecomarket-servicio-pagos:qa"
-CONTAINER_NAME="ecomarket-servicio-pagos-qa"
-
-# Cargar la imagen Docker
-docker load -i $DOCKER_IMAGE_PATH
+CONTAINER_NAME="ecomarket-servicio-pagos"
+NETWORK_NAME="ecomarket-network"
 
 # Detener y eliminar el contenedor anterior si existe
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
+# Descargar la imagen desde Docker Hub
+docker pull $DOCKER_IMAGE_NAME
+
 # Ejecutar un nuevo contenedor
-docker run -d --name $CONTAINER_NAME -p 8081:8081 $DOCKER_IMAGE_NAME
+docker run -d --name $CONTAINER_NAME --network $NETWORK_NAME -p 8081:8081 $DOCKER_IMAGE_NAME
 
-# Opcional: eliminar el archivo .tar para ahorrar espacio
-rm $DOCKER_IMAGE_PATH
-
-echo "Deployment ecomarket-servicio-pagos-qa completed successfully!"
-
+echo "Deployment ecomarket-servicio-pagos completed successfully!"
