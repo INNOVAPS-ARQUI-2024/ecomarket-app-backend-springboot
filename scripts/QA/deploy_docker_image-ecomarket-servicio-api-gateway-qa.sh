df64fa9b791d29ad0aaf13138130f32a3647621b
Daniel Felipe Alzate <117049972+XxDaniel-AlzatexX@users.noreply.github.com>
2024-10-17 21:01:41 -0500

    Update deploy_docker_image-ecomarket-servicio-api-gateway-qa.sh

--- scripts/QA/deploy_docker_image-ecomarket-servicio-api-gateway-qa.sh
+++ scripts/QA/deploy_docker_image-ecomarket-servicio-api-gateway-qa.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 # Variables
-DOCKER_IMAGE_PATH="/tmp/ecomarket-servicio-api-gateway-qa.tar"
 DOCKER_IMAGE_NAME="ecomarket-servicio-api-gateway:qa"
-CONTAINER_NAME="ecomarket-servicio-api-gateway-qa"
+CONTAINER_NAME="ecomarket-servicio-api-gateway"
 NETWORK_NAME="ecomarket-network"
-
-# Cargar la imagen Docker
-docker load -i $DOCKER_IMAGE_PATH
 
 # Detener y eliminar el contenedor anterior si existe
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
-# Ejecutar un nuevo contenedor en la red
-docker run -d --name $CONTAINER_NAME --network $NETWORK_NAME -p 7999:7999 $DOCKER_IMAGE_NAME
+# Descargar la imagen desde Docker Hub
+docker pull $DOCKER_IMAGE_NAME
 
-# Opcional: eliminar el archivo .tar para ahorrar espacio
-rm $DOCKER_IMAGE_PATH
+# Ejecutar un nuevo contenedor
+docker run -d --name $CONTAINER_NAME --network $NETWORK_NAME -p 7999:7999 $DOCKER_IMAGE_NAME
 
 echo "Deployment ecomarket-servicio-api-gateway-qa completed successfully!"
