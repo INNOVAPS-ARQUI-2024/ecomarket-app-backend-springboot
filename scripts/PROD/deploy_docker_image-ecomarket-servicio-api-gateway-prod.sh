8c594c28db8a666b62e9a90e0df5b6642b5683b0
Daniel Felipe Alzate <117049972+XxDaniel-AlzatexX@users.noreply.github.com>
2024-10-17 21:49:12 -0500

    Update deploy_docker_image-ecomarket-servicio-api-gateway-prod.sh

--- scripts/PROD/deploy_docker_image-ecomarket-servicio-api-gateway-prod.sh
+++ scripts/PROD/deploy_docker_image-ecomarket-servicio-api-gateway-prod.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-# Variables (puedes personalizarlas o recibirlas como parámetros)
-DOCKER_IMAGE_PATH="/tmp/ecomarket-servicio-api-gateway-prod.tar"
-DOCKER_IMAGE_NAME="ecomarket-servicio-api-gateway:prod"
-CONTAINER_NAME="ecomarket-servicio-api-gateway-prod"
-
-# Cargar la imagen Docker
-docker load -i $DOCKER_IMAGE_PATH
+# Variables
+DOCKER_IMAGE_NAME="devopsarqui2024/ecomarket-servicio-api-gateway:prod"
+CONTAINER_NAME="ecomarket-servicio-api-gateway"
+NETWORK_NAME="ecomarket-network"
 
 # Detener y eliminar el contenedor anterior si existe
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
+# Descargar la imagen desde Docker Hub
+docker pull $DOCKER_IMAGE_NAME
+
 # Ejecutar un nuevo contenedor
-docker run -d --name $CONTAINER_NAME -p 7999:7999 $DOCKER_IMAGE_NAME
+docker run -d --name $CONTAINER_NAME --network $NETWORK_NAME -p 7999:7999 $DOCKER_IMAGE_NAME
 
-# Opcional: eliminar el archivo .tar para ahorrar espacio
-rm $DOCKER_IMAGE_PATH
-
-echo "Deployment ecomarket-servicio-api-gateway-prod completed successfully!"
+echo "Deployment ecomarket-servicio-api-gateway completed successfully!"
