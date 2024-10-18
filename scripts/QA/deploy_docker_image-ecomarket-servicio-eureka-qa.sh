e670c1398855f736f3ae2dd2c56fb849a350a6c9
Daniel Felipe Alzate <117049972+XxDaniel-AlzatexX@users.noreply.github.com>
2024-10-17 21:04:17 -0500

    Update deploy_docker_image-ecomarket-servicio-eureka-qa.sh

--- scripts/QA/deploy_docker_image-ecomarket-servicio-eureka-qa.sh
+++ scripts/QA/deploy_docker_image-ecomarket-servicio-eureka-qa.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 # Variables
-DOCKER_IMAGE_PATH="/tmp/ecomarket-servicio-eureka-qa.tar"
 DOCKER_IMAGE_NAME="ecomarket-servicio-eureka:qa"
-CONTAINER_NAME="ecomarket-servicio-eureka-qa"
+CONTAINER_NAME="ecomarket-servicio-eureka"
 NETWORK_NAME="ecomarket-network"
-
-# Cargar la imagen Docker
-docker load -i $DOCKER_IMAGE_PATH
 
 # Detener y eliminar el contenedor anterior si existe
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
+# Descargar la imagen desde Docker Hub
+docker pull $DOCKER_IMAGE_NAME
+
 # Ejecutar un nuevo contenedor en la red
 docker run -d --name $CONTAINER_NAME --network $NETWORK_NAME -p 8761:8761 $DOCKER_IMAGE_NAME
-
-# Opcional: eliminar el archivo .tar para ahorrar espacio
-rm $DOCKER_IMAGE_PATH
 
 echo "Deployment ecomarket-servicio-eureka-qa completed successfully!"
