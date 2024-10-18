74924b1a5d621529230e82ce0e9eddd8cc4dc604
Daniel Felipe Alzate <117049972+XxDaniel-AlzatexX@users.noreply.github.com>
2024-10-17 21:11:31 -0500

    Update deploy_docker_image-ecomarket-servicio-ofertas-qa.sh

--- scripts/QA/deploy_docker_image-ecomarket-servicio-ofertas-qa.sh
+++ scripts/QA/deploy_docker_image-ecomarket-servicio-ofertas-qa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Variables
-DOCKER_IMAGE_NAME="ecomarket-servicio-ofertas:qa"
+DOCKER_IMAGE_NAME="devopsarqui2024/ecomarket-servicio-ofertas:qa"
 CONTAINER_NAME="ecomarket-servicio-ofertas"
 NETWORK_NAME="ecomarket-network"
 
@@ -15,4 +15,4 @@ docker pull $DOCKER_IMAGE_NAME
 # Ejecutar un nuevo contenedor
 docker run -d --name $CONTAINER_NAME --network $NETWORK_NAME -p 8083:8083 $DOCKER_IMAGE_NAME
 
-echo "Deployment ecomarket-servicio-ofertas-qa completed successfully!"
+echo "Deployment ecomarket-servicio-ofertas completed successfully!"
