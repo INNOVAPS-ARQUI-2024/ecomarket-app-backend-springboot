c5f3722ac9bbec5255064cf52f0b1d96e61b66e9
Daniel Felipe Alzate <117049972+XxDaniel-AlzatexX@users.noreply.github.com>
2024-10-17 21:49:35 -0500

    Update deploy_docker_image-ecomarket-servicio-control-usuarios-prod.sh

--- scripts/PROD/deploy_docker_image-ecomarket-servicio-control-usuarios-prod.sh
+++ scripts/PROD/deploy_docker_image-ecomarket-servicio-control-usuarios-prod.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 # Variables
-DOCKER_IMAGE_PATH="/tmp/ecomarket-servicio-control-usuarios-prod.tar"
-DOCKER_IMAGE_NAME="ecomarket-servicio-control-usuarios:prod"
-CONTAINER_NAME="ecomarket-servicio-control-usuarios-prod"
-
-# Cargar la imagen Docker
-docker load -i $DOCKER_IMAGE_PATH
+DOCKER_IMAGE_NAME="devopsarqui2024/ecomarket-servicio-control-usuarios:prod"
+CONTAINER_NAME="ecomarket-servicio-control-usuarios"
+NETWORK_NAME="ecomarket-network"
 
 # Detener y eliminar el contenedor anterior si existe
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
+# Descargar la imagen desde Docker Hub
+docker pull $DOCKER_IMAGE_NAME
+
 # Ejecutar un nuevo contenedor
-docker run -d --name $CONTAINER_NAME -p 8082:8082 $DOCKER_IMAGE_NAME
+docker run -d --name $CONTAINER_NAME --network $NETWORK_NAME -p 8082:8082 $DOCKER_IMAGE_NAME
 
-# Opcional: eliminar el archivo .tar para ahorrar espacio
-rm $DOCKER_IMAGE_PATH
-
-echo "Deployment ecomarket-servicio-control-usuarios-prod completed successfully!"
-
+echo "Deployment ecomarket-servicio-control-usuarios completed successfully!"
