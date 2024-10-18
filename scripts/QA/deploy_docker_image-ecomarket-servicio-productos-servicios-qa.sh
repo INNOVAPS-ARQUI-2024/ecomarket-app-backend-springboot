9e3a58d23164fec71318fed8e42a70e65e5b1ec1
Daniel Felipe Alzate <117049972+XxDaniel-AlzatexX@users.noreply.github.com>
2024-10-17 21:08:53 -0500

    Update deploy_docker_image-ecomarket-servicio-productos-servicios-qa.sh

--- scripts/QA/deploy_docker_image-ecomarket-servicio-productos-servicios-qa.sh
+++ scripts/QA/deploy_docker_image-ecomarket-servicio-productos-servicios-qa.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 # Variables
-DOCKER_IMAGE_PATH="/tmp/ecomarket-servicio-productos-servicios-qa.tar"
 DOCKER_IMAGE_NAME="ecomarket-servicio-productos-servicios:qa"
 CONTAINER_NAME="ecomarket-servicio-productos-servicios-qa"
 NETWORK_NAME="ecomarket-network"
-
-# Cargar la imagen Docker
-docker load -i $DOCKER_IMAGE_PATH
 
 # Detener y eliminar el contenedor anterior si existe
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
-# Ejecutar un nuevo contenedor en la red (sin exponer puerto al host)
-docker run -d --name $CONTAINER_NAME --network $NETWORK_NAME $DOCKER_IMAGE_NAME
+# Descargar la imagen desde Docker Hub
+docker pull $DOCKER_IMAGE_NAME
 
-# Opcional: eliminar el archivo .tar para ahorrar espacio
-rm $DOCKER_IMAGE_PATH
+# Ejecutar un nuevo contenedor
+docker run -d --name $CONTAINER_NAME --network $NETWORK_NAME $DOCKER_IMAGE_NAME -p 8080:8080 $DOCKER_IMAGE_NAME
 
-echo "Deployment ecomarket-servicio-productos-servicios-qa completed successfully!"
+echo "Deployment ecomarket-servicio-productos-servicios completed successfully!"
