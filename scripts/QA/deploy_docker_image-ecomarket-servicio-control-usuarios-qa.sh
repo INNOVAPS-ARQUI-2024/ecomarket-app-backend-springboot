31793267574afaff529a89754071506688d335b9
Daniel Felipe Alzate <117049972+XxDaniel-AlzatexX@users.noreply.github.com>
2024-10-17 21:10:25 -0500

    Update deploy_docker_image-ecomarket-servicio-control-usuarios-qa.sh

--- scripts/QA/deploy_docker_image-ecomarket-servicio-control-usuarios-qa.sh
+++ scripts/QA/deploy_docker_image-ecomarket-servicio-control-usuarios-qa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Variables
-DOCKER_IMAGE_NAME="ecomarket-servicio-control-usuarios:qa"
+DOCKER_IMAGE_NAME="devopsarqui2024/ecomarket-servicio-control-usuarios:qa"
 CONTAINER_NAME="ecomarket-servicio-control-usuarios"
 NETWORK_NAME="ecomarket-network"
 
@@ -15,4 +15,4 @@ docker pull $DOCKER_IMAGE_NAME
 # Ejecutar un nuevo contenedor
 docker run -d --name $CONTAINER_NAME --network $NETWORK_NAME -p 8082:8082 $DOCKER_IMAGE_NAME
 
-echo "Deployment ecomarket-servicio-control-usuarios-qa completed successfully!"
+echo "Deployment ecomarket-servicio-control-usuarios completed successfully!"
