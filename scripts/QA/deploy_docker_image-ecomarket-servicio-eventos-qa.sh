6c4a9451f337b9f9592181820abed377648fa1e9
Daniel Felipe Alzate <117049972+XxDaniel-AlzatexX@users.noreply.github.com>
2024-10-17 21:05:08 -0500

    Update deploy_docker_image-ecomarket-servicio-eventos-qa.sh

--- scripts/QA/deploy_docker_image-ecomarket-servicio-eventos-qa.sh
+++ scripts/QA/deploy_docker_image-ecomarket-servicio-eventos-qa.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 # Variables
-DOCKER_IMAGE_PATH="/tmp/ecomarket-servicio-eventos-qa.tar"
 DOCKER_IMAGE_NAME="ecomarket-servicio-eventos:qa"
-CONTAINER_NAME="ecomarket-servicio-eventos-qa"
-
-# Cargar la imagen Docker
-docker load -i $DOCKER_IMAGE_PATH
+CONTAINER_NAME="ecomarket-servicio-eventos"
+NETWORK_NAME="ecomarket-network"
 
 # Detener y eliminar el contenedor anterior si existe
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
-# Ejecutar un nuevo contenedor
-docker run -d --name $CONTAINER_NAME -p 8084:8084 $DOCKER_IMAGE_NAME
+# Descargar la imagen desde Docker Hub
+docker pull $DOCKER_IMAGE_NAME
 
-# Opcional: eliminar el archivo .tar para ahorrar espacio
-rm $DOCKER_IMAGE_PATH
+# Ejecutar un nuevo contenedor
+docker run -d --name $CONTAINER_NAME --network $NETWORK_NAME -p 8084:8084 $DOCKER_IMAGE_NAME
 
 echo "Deployment ecomarket-servicio-eventos-qa completed successfully!"
-
