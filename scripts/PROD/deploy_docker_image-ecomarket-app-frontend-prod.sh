9798b7bc9f4e4ac7fd31b1c86ff699b1ecc4994d
Daniel Felipe Alzate <117049972+XxDaniel-AlzatexX@users.noreply.github.com>
2024-10-17 21:48:46 -0500

    Update deploy_docker_image-ecomarket-app-frontend-prod.sh

--- scripts/PROD/deploy_docker_image-ecomarket-app-frontend-prod.sh
+++ scripts/PROD/deploy_docker_image-ecomarket-app-frontend-prod.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 # Variables
-DOCKER_IMAGE_PATH="/tmp/ecomarket-app-frontend-prod.tar"
-DOCKER_IMAGE_NAME="ecomarket-app-frontend:prod"
-CONTAINER_NAME="ecomarket-app-frontend-prod"
-
-# Cargar la imagen Docker
-docker load -i $DOCKER_IMAGE_PATH
+DOCKER_IMAGE_NAME="devopsarqui2024/ecomarket-app-frontend:prod"
+CONTAINER_NAME="ecomarket-app-frontend"
+NETWORK_NAME="ecomarket-network"
 
 # Detener y eliminar el contenedor anterior si existe
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
+# Descargar la imagen desde Docker Hub
+docker pull $DOCKER_IMAGE_NAME
+
 # Ejecutar un nuevo contenedor
-docker run -d --name $CONTAINER_NAME -p 80:80 $DOCKER_IMAGE_NAME
+docker run -d --name $CONTAINER_NAME --network $NETWORK_NAME -p 80:80 $DOCKER_IMAGE_NAME
 
-# Opcional: eliminar el archivo .tar para ahorrar espacio
-rm $DOCKER_IMAGE_PATH
-
-echo "Deployment ecomarket-app-frontend-prod completed successfully!"
-
+echo "Deployment ecomarket-app-frontend completed successfully!"
